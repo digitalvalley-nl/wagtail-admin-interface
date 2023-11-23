4e5fc36dec2267a08d77aca1ca5d664037c85996
David Conde <david@digitalvalley.nl>
2023-11-23 23:38:20 +0100

    Integrated deployment pipeline into open source package repository Implemented customizability of admin interface for handling custom permission views Implemented customizability of admin interface for handling main menu items Implemented customizability of admin interface for handling settings menu items

---
+++ README.md
@@ -0,0 +1,1 @@
+# Wagtail Admin Interface
---
+++ wagtail_admin_interface/wagtail_hooks.py
@@ -0,0 +1,46 @@
+# Python Standard Library
+import json
+
+# Django
+from django.conf import settings
+from django.contrib.auth.models import Permission
+from django.utils.safestring import mark_safe
+
+# Wagtail
+from wagtail import hooks
+from wagtail.models import Page
+
+
+@hooks.register('construct_main_menu')
+def construct_main_menu(request, menu_items):
+    if not hasattr(settings, 'WAGTAIL_ADMIN_INTERFACE'):
+        return
+
+    # Hide main menu items that are not whitelisted
+    if 'HIDDEN_MAIN_MENU_ITEMS' in settings.WAGTAIL_ADMIN_INTERFACE:
+        menu_items[:] = [menu_item for menu_item in menu_items if menu_item.name not in settings.WAGTAIL_ADMIN_INTERFACE['HIDDEN_MAIN_MENU_ITEMS']]
+
+
+@hooks.register('construct_settings_menu')
+def construct_settings_menu(request, menu_items):
+    if not hasattr(settings, 'WAGTAIL_ADMIN_INTERFACE'):
+        return
+
+    # Hide settings menu items that are not whitelisted
+    if 'HIDDEN_SETTINGS_MENU_ITEMS' in settings.WAGTAIL_ADMIN_INTERFACE:
+        menu_items[:] = [menu_item for menu_item in menu_items if menu_item.name not in settings.WAGTAIL_ADMIN_INTERFACE['HIDDEN_SETTINGS_MENU_ITEMS']]
+
+
+@hooks.register('insert_global_admin_js')
+def insert_global_admin_js():
+    data = {}
+
+    if not hasattr(settings, 'WAGTAIL_ADMIN_INTERFACE'):
+        return
+
+    # Hide group object permisions that are not whitelisted
+    if 'HIDDEN_GROUP_OBJECT_PERMISSIONS' in settings.WAGTAIL_ADMIN_INTERFACE:
+        permissions = Permission.objects.filter(content_type__model__in=settings.WAGTAIL_ADMIN_INTERFACE['HIDDEN_GROUP_OBJECT_PERMISSIONS'])
+        data['HIDDEN_GROUP_OBJECT_PERMISSIONS'] = [permission.id for permission in permissions]
+
+    return mark_safe('<script>const WagtailAdminInterface = ' + json.dumps(data)  + ';</script>')
